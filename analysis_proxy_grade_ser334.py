7667e3e4f62c463417cb7dc8471854702813bb10
Ruben Acuña <racuna1@asu.edu>
2026-02-23 20:19:33 -0700

    Added support for proxy grades for SER334 M3, M6, and M9.

--- analysis_proxy_grade_ser334.py
+++ analysis_proxy_grade_ser334.py
@@ -4,7 +4,8 @@ shoggoth-validation - analysis_proxy_grade_ser334.py
 See analysis_proxy_grade_ser222.py for details.
 """
 __author__ = "Ruben Acuna"
-__copyright__ = "Copyright 2025, Ruben Acuna"
+__author__ = "Kyle Burnett"
+__author__ = "Michael Deitch"
 
 from analysis_proxy_grade_util import was_test_passed_by_name
 
@@ -134,29 +135,434 @@ def compute_proxies_m3_24fc(data):
     # Novice: Some mistakes in creating structures for the headers of a BMP file or creating functions which read and write them.
     # No Marks: Most or all functionality is not attempted or is mostly incorrect.
 
-    #t1_1 = was_test_passed_by_name(data, "[1.1]") # DIB Headers IO [Hint: incorrectly sized DIB header struct (remove this)]
-    t1_2 = was_test_passed_by_name(data, "[1.2]") # BMP Headers IO [Hint: incorrectly reading and/or writing of BMP header]
-    t1_3 = was_test_passed_by_name(data,"[1.3]")  # DIB Headers IO [Hint: incorrectly reading and/or writing of DIB header]
+    t1_1 = was_test_passed_by_name(data, "1.1)") # "1.1) BMP Headers IO [Hint: incorrectly reading and/or writing of BMP header]"
+    t1_2 = was_test_passed_by_name(data, "1.2)") # 1.2) DIB Headers IO [Hint: incorrectly reading and/or writing of DIB header]
 
-    if t1_2 and t1_3:
+    if t1_1 and t1_2:
         proxies += [4.0]
-    elif t1_2 or t1_3:
+    elif t1_1 or t1_2:
+        proxies += [2.0]
+    else:
+        proxies += [0.0]
+        
+        
+    #2) Pixels IO (4pts)
+    numPassed = 0
+    t2_1 = was_test_passed_by_name(data, "2.1)") # 2.1) DIB Headers IO [Hint: incorrectly sized DIB header struct (change this to pixel struct size check)]
+    
+    t2_2 = was_test_passed_by_name(data, "2.2)") # "2.2) Reading pixels single row [Hint: Case when padding is needed]"
+    
+    t2_3 = was_test_passed_by_name(data, "2.3)") # "2.3) Reading pixels single rows [Hint: Case when padding is not needed"
+    
+    t2_4 = was_test_passed_by_name(data, "2.4)") # "2.4) Reading pixels many rows  [Hint: Case when padding is needed]"
+    
+    t2_5 = was_test_passed_by_name(data, "2.5)") # "2.5) Reading pixels many rows [Hint: Case when padding is not needed]"
+    
+    t3_1 = was_test_passed_by_name(data, "3.1)") # "3.1) Write pixels single row [Hint: Case when padding is needed]"
+    
+    t3_2 = was_test_passed_by_name(data, "3.2)") # "3.2) Write pixels single row [Hint: Case when padding is not needed]"
+    
+    t3_3 = was_test_passed_by_name(data, "3.3)") # "3.3) Write pixels many rows [Hint: Case when paddings is needed]"
+    
+    t3_4 = was_test_passed_by_name(data, "3.4)") # "3.4) Write pixels many rows [Hint: Case when paddings is not needed]"
+    
+    #Sorry for ugly code ._.
+    if (t2_1):
+        numPassed += 1
+    if (t2_2):
+        numPassed += 1
+    if (t2_3):
+        numPassed += 1
+    if (t2_4):
+        numPassed += 1
+    if (t2_5):
+        numPassed += 1
+    if (t3_1):
+        numPassed += 1
+    if (t3_2):
+        numPassed += 1
+    if (t3_3):
+        numPassed += 1
+    if (t3_4):
+        numPassed += 1
+    
+    if  (t2_1 and t2_2 and t2_3 and t2_4 and t2_5 and
+            t3_1 and t3_2 and t3_3 and t3_4):
+        proxies += [4.0]
+    elif numPassed >= 5:
+        proxies += [2.0]
+    else:
+        proxies += [0.0]
+    
+    
+    #3) Input and output file names (4pts)
+    t9_1 = was_test_passed_by_name(data, "9.1)") # "9.1) Program Input 1 [Hint: Default output file name is correct.]"
+    
+    t9_2 = was_test_passed_by_name(data, "9.2)") # "9.2) Program Input 2 [Hint: Output file matches argument.]"
+    
+    t9_3 = was_test_passed_by_name(data, "9.3)") # "9.3) Program Input 3 [Hint: Input File Does not Exist.]"
+    
+    if t9_1 and t9_2 and t9_3:
+        proxies += [4.0]
+    elif ((t9_1 and t9_2) or (t9_1 and t9_3) or (t9_2 and t9_3)):
+        proxies += [2.0]
+    else:
+        proxies += [0.0]
+    
+    
+    #4) Input Validation
+    t10_1 = was_test_passed_by_name(data, "10.1)") # "10.1) Program Input 4 [Hint: Color Arguments.]"
+    
+    if t10_1:
+        proxies += [4.0]
+    else:
+        proxies += [0.0]
+    
+    
+    #5) Filter: Color Shift (5pts)
+    t5_1 = was_test_passed_by_name(data, "5.1)") # "5.1) Color shift - Divisible by 4, shfiting 0. [Hint no changes to pixels]"
+    
+    t5_2 = was_test_passed_by_name(data, "5.2)") # "5.2) Color shift - Divisible by 4, shfiting positive. [Hint: Clamping and positive shifts]"
+    
+    t5_3 = was_test_passed_by_name(data, "5.3)") # "5.3) Color shift - Divisible by 4, shfiting negative. [Hint: Clamping and negative shifts]"
+    
+    t5_4 = was_test_passed_by_name(data, "5.4)") # "5.4) Color shift - Non-Divisible by 4, shfiting 0. [Hint no changes to pixels]"
+    
+    t5_5 = was_test_passed_by_name(data, "5.5)") # "5.5) Color shift - Non-Divisible by 4, shfiting positive. [Hint: Clamping and positive shifts]"
+    
+    t5_6 = was_test_passed_by_name(data, "5.6)") # "5.6) Color shift - Non-Divisible by 4, shfiting negative. [Hint: Clamping and negative shifts]"
+    
+    t10_1 = was_test_passed_by_name(data, "10.1)") # "10.1) Program Input 4 [Hint: Color Arguments.]"
+    
+    t10_2 = was_test_passed_by_name(data, "10.2)") # "10.2) Color Shift 1 [Hint: Basic Behavior Shifting Up.]"
+    
+    t10_3 = was_test_passed_by_name(data, "10.3)") # "10.3) Color Shift 2 [Hint: Basic Behavior Shifting Down.]"
+    
+    t10_4 = was_test_passed_by_name(data, "10.4)") # "10.4) Color Shift 3 [Hint: Shifting Multiple Colors Up.]"
+    
+    t10_5 = was_test_passed_by_name(data, "10.5)") # "10.5) Color Shift 4 [Hint: Shifting Multiple Colors Down.]"
+    
+    
+    colorOption1 = t5_2 and t5_5 and t10_2 and t10_4 and t5_1 and t5_4 # only able to shift up correctly
+    
+    colorOption2 = t5_3 and t5_6 and t10_3 and t10_5 and t5_1 and t5_4 # only able to shift down correctly
+    
+    colorOption3 = t5_1 and t5_2 and t5_3 # only able to shift divisible by 4 images
+    
+    colorOption4 = t5_4 and t5_5 and t5_6 # only able to shift non divisible by 4 images
+    
+    if (t5_1 and t5_2 and t5_3 and t5_4 and t5_5 and t5_6 and
+        t10_2 and t10_3 and t10_4 and t10_5):
+        proxies += [5.0]
+    elif (colorOption1 or colorOption2 or colorOption3 or colorOption4):
+        proxies += [2.5]
+    else:
+        proxies += [0.0]
+        
+        
+    #6) Test: Copy Image (5pts)
+    t11_1 = was_test_passed_by_name(data, "11.1)") # "11.1) Copy Image 1 [Hint: Copied Image matches size of original image.]"
+    
+    t11_2 = was_test_passed_by_name(data, "11.2)") # "11.2) Copy Image 2 [Hint: Copied Image has matching pixel array.]"
+    
+    t12_1 = was_test_passed_by_name(data, "12.1)") # "12.1) Copy Image 3 [Hint: Copied Image matches size of original image on image with padding.]"
+    
+    t12_2 = was_test_passed_by_name(data, "12.2)") # "12.2) Copy Image 4 [Hint: Copied Image has matching pixel array on image with padding.]"
+    
+    if (t11_1 and t11_2 and t12_1 and t12_2):
+        proxies += [5.0]
+    elif ((t11_1 and t11_2) or (t12_1 and t12_2) or (t11_1 and t12_1)):
+        proxies += [2.5]
+    else:
+        proxies += [0.0]
+    
+    
+    #7) Image OOP (5pts)
+    
+    t8_1 = was_test_passed_by_name(data, "8.1)") # "8.1) Image create [Hint: all fields set correctly]"
+    
+    t8_2 = was_test_passed_by_name(data, "8.2)") # "8.2) Image destroy [Hint: 'Object' should be no longer usable]"
+    
+    t8_3 = was_test_passed_by_name(data, "8.3)") # "8.3) Image destroy [Hint: Underlying pixel array should not be deallocated...]"
+    
+    t4_1 = was_test_passed_by_name(data, "4.1)") # "4.1) Image -- getWidth [Hint: Case when padding is not needed]"
+    
+    t4_2 = was_test_passed_by_name(data, "4.2)") # "4.2) Image -- getWidth [Hint: Case when there is padding present"
+    
+    t4_3 = was_test_passed_by_name(data, "4.3)") # "4.3) Image -- getHeight [Hint: Case when there is no padding present]"
+    
+    t4_4 = was_test_passed_by_name(data, "4.4)") # "4.4) Image -- getHeight [Hint: case when there is padding present]"
+    
+    t4_5 = was_test_passed_by_name(data, "4.5)") # "4.5) Image -- getPixels [Hint: padding necessary]"
+    
+    t4_6 = was_test_passed_by_name(data, "4.6)") # "4.6) Image -- getPixels [Hint: No padding needed]"
+    
+    t4_7 = was_test_passed_by_name(data, "4.7)") # "4.7) Image -- getPixels [Hint: No padding needed]"
+    
+    t4_8 = was_test_passed_by_name(data, "4.8)") #  "4.8) Image -- getPixels [Hint: No padding needed]"
+    
+    numPassed = 0
+    
+    if t4_1:
+        numPassed += 1
+    if t4_2:
+        numPassed += 1
+    if t4_3:
+        numPassed += 1
+    if t4_4:
+        numPassed += 1
+    if t4_5:
+        numPassed += 1
+    if t4_6:
+        numPassed += 1
+    if t4_7:
+        numPassed += 1
+    if t4_8:
+        numPassed += 1
+
+    if (t8_1 and t8_2 and t8_3 and 
+        t4_1 and t4_2 and t4_3 and t4_4 and t4_5 and
+        t4_6 and t4_7 and t4_8):
+        proxies += [5.0]
+    elif ((numPassed >= 4) or (t8_1 and t8_2 and t8_3) or 
+          (t4_1 and t4_2 and t4_3 and t4_4 and t4_5 and t4_6 and t4_7)):
+          proxies += [2.5]
+    else:
+        proxies += [0.0]
+        
+        
+    #8) Filter: Grayscale (5pts)
+    t6_1 = was_test_passed_by_name(data, "6.1)") # "6.1) Image_BW divisble by 4 [Hint: basic functionality]"
+    
+    t6_2 = was_test_passed_by_name(data, "6.2)") # "6.2) Image_BW -- non divisble by 4 [Hint: basic functionality]"
+    
+    t6_3 = was_test_passed_by_name(data, "6.3)") # "6.3) Image_BW -- Setting channels [Hint: Properties of grayscale ]"
+    
+    t13_1 = was_test_passed_by_name(data, "13.1)") # "13.1) Grayscale 1 [Hint: Basic Behavior.]"
+    
+    t13_2 = was_test_passed_by_name(data, "13.2)") # "13.2) Grayscale 2 [Hint: Grayscale applied before color shift.]"
+    
+    if t6_1 and t6_2 and t6_3 and t13_1 and t13_2:
+        proxies += [5.0]
+    elif ((t6_1 and t6_2 and t6_3) or (t6_1 and t6_3 and t13_1) or
+             (t6_2 and t6_3 and t13_1)):
+        proxies += [2.5]
+    else:
+        proxies += [0.0]
+    
+    
+    #9) Filter: Scaling
+    t7_1 = was_test_passed_by_name(data, "7.1)") # "7.1) Image -- Resize, number of pixels [Hint: scaling up]"
+
+    t7_2 = was_test_passed_by_name(data, "7.2)") # "7.2) Image -- Resize, pixel data [Hint: scaling up]"
+    
+    t7_3 = was_test_passed_by_name(data, "7.3)") # "7.3) Image -- Resize, number of pixels [Hint: scaling down]"
+    
+    t7_4 = was_test_passed_by_name(data, "7.4)") # "7.4) Image -- Resize, pixel data [Hint: scaling down]"
+    
+    t14_1 = was_test_passed_by_name(data, "14.1)") # "14.1) Image Scaling 1 [Hint: Scaled up image has correct image size.]"
+    
+    t14_2 = was_test_passed_by_name(data, "14.2)") # "14.2) Image Scaling 2 [Hint: Scaled up image has correct pixel array.]"
+    
+    t14_3 = was_test_passed_by_name(data, "14.3)") # "14.3) Image Scaling 3 [Hint: Scaled down image has correct image size.]"
+    
+    t14_4 = was_test_passed_by_name(data, "14.4)") # "14.4) Image Scaling 4 [Hint: Scaled down image has correct pixel array.]"
+    
+    if (t7_1 and t7_2 and t7_3 and t7_4 and
+        t14_1 and t14_2 and t14_3 and t14_4):
+        proxies += [5.0]
+    elif ((t7_1 and t7_2 and t14_1 and t14_2) or 
+          (t7_3 and t7_4 and t14_3 and t14_4)):
+        proxies += [2.5]
+    else:
+        proxies += [0.0]
+    
+    
+    
+    total_score_proxy = sum([ps for ps in proxies if ps])
+
+    return proxies, total_score_proxy
+
+def compute_proxies_m9_25fc(data):
+
+    proxies = []
+
+    # 1a) Read Data: Basic, Read Data: Process(es)
+    # The two first checks in the rubric will be covered by 'Read Data From File", simply because
+    # separating the two out is not very feasible.
+    # Presently, there's not a lot of space for partial credit, so these two going to end up being all or nothing.
+
+    t1_1 = was_test_passed_by_name(data, "Read Data From File")
+    t1_2 = was_test_passed_by_name(data, "Dynamic Memory Allocation")
+
+    if t1_1:
+        proxies += [2.0]
+        proxies += [2.0]
+    else:
+        proxies += [0.0]
+        proxies += [0.0]
+
+    # 1b) Read Data: Dynamic
+    if t1_2:
+        proxies += [7.0]
+    else:
+        proxies += [0.0]
+
+    # 2) SJF: Simulation
+    # Full credit if display and simulate, half if just simulate
+
+    t2_1 = was_test_passed_by_name(data, "SJF Test Display Ticks")
+    t2_2 = was_test_passed_by_name(data, "SJF Test Simulate Ticks")
+
+    if t2_1 and t2_2:
+        proxies += [3.0]
+    elif t2_1:
+        proxies += [1.5]
+    else:
+        proxies += [0.0]
+
+    # 3) SJF: Algorithms
+    # Full credit if the algorithm works properly for any number of processes.
+    # Partial credit if it only works for two processes
+
+    t2_3 = was_test_passed_by_name(data, "SJF Test Algorithm with 2 Processes")
+    t2_4 = was_test_passed_by_name(data, "SJF Test Algorithm with a variable number of Processes")
+
+    if t2_3 and t2_4:
+        proxies += [4.0]
+    elif t2_3: # semi-match: correctly supports 2, but does not require all.
         proxies += [2.0]
     else:
         proxies += [0.0]
 
-    #TODO: complete below
+    # 4) SJF: Output
+    # Full credit if both turnaround and waiting time are correct
+    # Partial credit if just one is correct
 
-    #2) Pixels IO
-    #3) Input and output file names
-    #4) Input Validation
-    #5) Filter: Color Shift
-    #6) Test: Copy Image
-    #7) Image OOP
-    #8) Filter: Grayscale
-    #9) Filter: Scaling
+    t2_5 = was_test_passed_by_name(data, "SJF Calculate Turnaround Time")
+    t2_6 = was_test_passed_by_name(data, "SJF Calculate Waiting Time")
+
+    if t2_5 and t2_6:
+        proxies += [3.0]
+    elif t2_5 or t2_6:
+        proxies += [1.5]
+    else:
+        proxies += [0.0]
+
+    # 5) SJFL: Simulation
+    # Full credit if display and simulate, half if just simulate
+
+    t3_1 = was_test_passed_by_name(data, "SJFL Test Display Ticks")
+    t3_2 = was_test_passed_by_name(data, "SJFL Test Simulate Ticks")
+
+    if t3_1 and t3_2:
+        proxies += [3.0]
+    elif t3_1:
+        proxies += [1.5]
+    else:
+        proxies += [0.0]
+
+    # 6) SJFL: Algorithms
+    # Full credit if the algorithm works properly for any number of processes.
+    # Partial credit if it only works for two processes
+
+    t3_3 = was_test_passed_by_name(data, "SJFL Test Algorithm with 2 Processes")
+    t3_4 = was_test_passed_by_name(data, "SJFL Test Algorithm with a variable number of Processes")
+
+    if t3_3 and t3_4:
+        proxies += [4.0]
+    elif t3_3: # semi-match: correctly supports 2, but does not require all.
+        proxies += [2.0]
+    else:
+        proxies += [0.0]
+
+    # 7) SJFL: Output
+    # Full credit if turnaround, waiting time, and estimation error are correct
+    # Partial credit if just one is correct
+
+    t3_5 = was_test_passed_by_name(data, "SJFL Calculate Turnaround Time")
+    t3_6 = was_test_passed_by_name(data, "SJFL Calculate Waiting Time")
+    t3_7 = was_test_passed_by_name(data, "SJFL Calculate Estimation Error")
+
+    if t3_5 and t3_6 and t3_7:
+        proxies += [4.0]
+    elif t3_5 or t3_6 or t3_7:
+        proxies += [2.0]
+    else:
+        proxies += [0.0]
 
 
-    total_score_proxy = sum([ps for ps in proxies if ps])
+def compute_proxies_m6_25fc(data):
+    
+    proxies = []
 
-    return proxies, total_score_proxy
+    # 1) Command Args
+    # If the program accepts arguments and outputs some image, 
+    # both input filename and output filename are assumed to pass.
+    # However, it's tricky to give partial credit here without 
+    # splitting up some tests in the autograder.
+    # Also, because these tests more directly correspond to filter selection,
+    # they'll be used for those with 1 test being half credit and both being full.
+    # This also doesn't really line up with the original rubric, but it's the best 
+    # that can be done.
+    # If we care, this should be remedied by changing the actual tests and splitting up
+    # some of the checks to more closely align with the rubric.
+
+    t1_1 = was_test_passed_by_name(data, "Command Line 1")
+    t1_2 = was_test_passed_by_name(data, "Command Line 2")
+
+    #Input Filename and Output Filename
+    if t1_1 and t1_2:
+        proxies += [1.0]
+        proxies += [1.0]
+    
+    # 2) Blur Filter
+    # For the Blur Algorithm Points: Partial credit if one of the two passes, full credit if both do
+    # For the rest of the memory stuff, we don't have tests for that in my autograder, so just auto fail
+
+    t2_1 = was_test_passed_by_name(data, "Blur Interior Pixels")
+    t2_2 = was_test_passed_by_name(data, "Blur Exterior Pixels")
+
+    # Blur Algorithm
+    if t2_1 and t2_2:
+        proxies += [4.0]
+    elif t2_1 or t2_2:
+        proxies += [2.0]
+    else:
+        proxies += [0.0]
+    
+    # Automatic failures for:
+    proxies += [0.0] # Blur: Threaded Algorithm
+    proxies += [0.0] # Blur: Balanced Algorithm
+    proxies += [0.0] # Blur: Independent Memory
+    proxies += [0.0] # Blur: Padded Memory
+
+    # 3) Cheese Filter
+    # Mapping color algorithm is simple as it's a one to one for "Tint Image Yellow"
+    # The hole algorithm, comprised of many tests in the autograder, will give partial credit if
+    #   the "Add Holes" test passes, and full credit if they all pass
+    # Like before, all of the dynamic memory related tests automatically fail
+
+    t3_1 = was_test_passed_by_name(data, "Tint Image Yellow")
+    t3_2 = was_test_passed_by_name(data, "Add Holes")
+
+    t3_3 = was_test_passed_by_name(data, "Holes are Proper Shape")
+    t3_4 = was_test_passed_by_name(data, "Holes are Proper Size")
+    t3_5 = was_test_passed_by_name(data, "Correct Number of Holes")
+    t3_6 = was_test_passed_by_name(data, "Holes are Uniformly Distributed")
+
+    if t3_1:
+        proxies += [4.0]
+    else:
+        proxies += [0.0]
+
+    if t3_2 and t3_3 and t3_4 and t3_5 and t3_6:
+        proxies += [6.0]
+    elif t3_2:
+        proxies += [3.0]
+    else:
+        proxies += [0.0]
+
+    proxies += [0.0] # Swiss Cheese: Threaded Algorithm
+    proxies += [0.0] # Swiss Cheese: Data Splitting Algorithm
+    proxies += [0.0] # Swiss Cheese: Independent Memory
